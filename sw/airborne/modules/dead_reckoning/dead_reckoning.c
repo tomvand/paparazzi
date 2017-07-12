3bcb9ca19e602e56c36761427f149b7f72d922c8
Tom van Dijk <tomvand@users.noreply.github.com>
2017-07-12 15:06:24 +0200

    Initialze new dead_reckoning module

---
+++ sw/airborne/modules/dead_reckoning/dead_reckoning.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) Tom van Dijk
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/dead_reckoning/dead_reckoning.c"
+ * @author Tom van Dijk
+ * Estimate quadrotor position from IMU data.
+ */
+
+#include "modules/dead_reckoning/dead_reckoning.h"
+
+// void dr_init {}
+
+
---
+++ sw/airborne/modules/dead_reckoning/dead_reckoning.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) Tom van Dijk
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/dead_reckoning/dead_reckoning.h"
+ * @author Tom van Dijk
+ * Estimate quadrotor position from IMU data.
+ */
+
+#ifndef DEAD_RECKONING_H
+#define DEAD_RECKONING_H
+
+// extern void dr_init;
+
+#endif
+
