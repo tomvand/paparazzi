18e7c5b834db65b23230300aff746f975687da55
Tom van Dijk <tomvand@users.noreply.github.com>
2018-05-18 17:57:57 +0200

    Attempt at threaded file logger. Does not work.

--- sw/airborne/modules/loggers/file_logger.c
+++ sw/airborne/modules/loggers/file_logger.c
@@ -32,6 +32,8 @@
 #include <stdio.h>
 #include "std.h"
 
+#include <pthread.h>
+
 #include "subsystems/imu.h"
 #include "firmwares/rotorcraft/stabilization.h"
 #include "state.h"
@@ -43,40 +45,40 @@
 
 /** Select variables to log */
 #ifndef FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
-#define FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE FALSE
+#define FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_AUTOPILOT
-#define FILE_LOGGER_LOG_AUTOPILOT FALSE
+#define FILE_LOGGER_LOG_AUTOPILOT TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_ATTITUDE
-#define FILE_LOGGER_LOG_ATTITUDE FALSE
+#define FILE_LOGGER_LOG_ATTITUDE TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_LTP_POS
-#define FILE_LOGGER_LOG_LTP_POS FALSE
+#define FILE_LOGGER_LOG_LTP_POS TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_LTP_VEL
-#define FILE_LOGGER_LOG_LTP_VEL FALSE
+#define FILE_LOGGER_LOG_LTP_VEL TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_BODY_VEL
-#define FILE_LOGGER_LOG_BODY_VEL FALSE
+#define FILE_LOGGER_LOG_BODY_VEL TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_SONAR_BEBOP
-#define FILE_LOGGER_LOG_SONAR_BEBOP FALSE
+#define FILE_LOGGER_LOG_SONAR_BEBOP TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_BARO
-#define FILE_LOGGER_LOG_BARO FALSE
+#define FILE_LOGGER_LOG_BARO TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_BATTERY
-#define FILE_LOGGER_LOG_BATTERY FALSE
+#define FILE_LOGGER_LOG_BATTERY TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE
-#define FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE FALSE
+#define FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE
-#define FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE FALSE
+#define FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE TRUE
 #endif
 #ifndef FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
-#define FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS FALSE
+#define FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS TRUE
 #endif
 
 #if FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
@@ -102,15 +104,86 @@
 #endif
 
 
-/** The file pointer */
-static FILE *file_logger = NULL;
+/** Log frame buffer */
+struct log_frame_t {
+  uint32_t counter;
+  float time;
+  float dt;
+#if FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
+  uint8_t block;
+  uint8_t stage;
+#endif
+#if FILE_LOGGER_LOG_AUTOPILOT
+  uint8_t ap_mode;
+  bool ap_motors_on;
+  bool ap_kill_throttle;
+  bool ap_in_flight;
+  bool ap_ground_detected;
+#endif
+#if FILE_LOGGER_LOG_ATTITUDE
+  struct FloatEulers att;
+#endif
+#if FILE_LOGGER_LOG_LTP_POS
+  struct NedCoor_f ltp_pos;
+#endif
+#if FILE_LOGGER_LOG_LTP_VEL
+  struct NedCoor_f ltp_vel;
+#endif
+#if FILE_LOGGER_LOG_BODY_VEL
+  struct FloatVect3 body_vel;
+#endif
+#if FILE_LOGGER_LOG_SONAR_BEBOP
+  float sonar_bebop;
+#endif
+#if FILE_LOGGER_LOG_BARO
+  bool baro_valid;
+  float baro;
+#endif
+#if FILE_LOGGER_LOG_BATTERY
+  float vsupply;
+  float current;
+  bool bat_low;
+  bool bat_crit;
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE || \
+    FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE || \
+    FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
+  bool percevite_ok;
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE
+  struct FloatVect3 percevite_vel;
+  float percevite_time_since_vel;
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE
+  float percevite_safe_dist;
+  uint32_t percevite_safe_dist_seq;
+  float percevite_raw_dist;
+  float percevite_valid_px;
+  float percevite_time_since_dist;
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
+  uint8_t percevite_wp_id;
+  struct NedCoor_f percevite_wp_pos;
+  uint8_t percevite_tgt_id;
+  struct NedCoor_f percevite_tgt_pos;
+#endif
+};
+static struct log_frame_t log_frame;
 
-/** Start the file logger and open a new file */
-void file_logger_start(void)
-{
+static pthread_t log_thread; ///< Logging thread
+static bool log_frame_requested = FALSE; ///< TRUE if periodic should fill log buffer
+static bool log_thread_is_running = FALSE; ///< TRUE while log thread is running
+static bool log_thread_should_run = FALSE; ///< TRUE as long as log thread should run
+
+/** Logging THREAD */
+void *file_logger_thread(void *arg);
+void *file_logger_thread(void *arg) {
+  log_thread_is_running = TRUE;
+  FILE *file_logger = NULL;
+
+  /* Open file */
   uint32_t counter = 0;
   char filename[512];
-
   // Check for available files
   sprintf(filename, "%s/%05d.csv", STRINGIFY(FILE_LOGGER_PATH), counter);
   while ((file_logger = fopen(filename, "r"))) {
@@ -119,159 +192,228 @@ void file_logger_start(void)
     counter++;
     sprintf(filename, "%s/%05d.csv", STRINGIFY(FILE_LOGGER_PATH), counter);
   }
-
   file_logger = fopen(filename, "w");
+  if(!file_logger) {
+    log_thread_is_running = FALSE;
+    return NULL;
+  }
 
-  if (file_logger != NULL) {
-    fprintf(file_logger, "counter,time,");
+  fprintf(file_logger, "counter,time,dt,");
 #if FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
-    fprintf(file_logger, "block,stage,");
+  fprintf(file_logger, "block,stage,");
 #endif
 #if FILE_LOGGER_LOG_AUTOPILOT
-    fprintf(file_logger, "ap_mode,ap_motors_on,ap_kill_throttle,ap_in_flight,ap_ground_detected,");
+  fprintf(file_logger, "ap_mode,ap_motors_on,ap_kill_throttle,ap_in_flight,ap_ground_detected,");
 #endif
 #if FILE_LOGGER_LOG_ATTITUDE
-    fprintf(file_logger, "att_phi,att_theta,att_psi,");
+  fprintf(file_logger, "att_phi,att_theta,att_psi,");
 #endif
 #if FILE_LOGGER_LOG_LTP_POS
-    fprintf(file_logger, "pos_ltp_x,pos_ltp_y,pos_ltp_z,");
+  fprintf(file_logger, "pos_ltp_x,pos_ltp_y,pos_ltp_z,");
 #endif
 #if FILE_LOGGER_LOG_LTP_VEL
-    fprintf(file_logger, "vel_ltp_x,vel_ltp_y,vel_ltp_z,");
+  fprintf(file_logger, "vel_ltp_x,vel_ltp_y,vel_ltp_z,");
 #endif
 #if FILE_LOGGER_LOG_BODY_VEL
-    fprintf(file_logger, "vel_body_x,vel_body_y,vel_body_z,");
+  fprintf(file_logger, "vel_body_x,vel_body_y,vel_body_z,");
 #endif
 #if FILE_LOGGER_LOG_SONAR_BEBOP
-    fprintf(file_logger, "sonar_bebop,");
+  fprintf(file_logger, "sonar_bebop,");
 #endif
 #if FILE_LOGGER_LOG_BARO
-    fprintf(file_logger, "baro_valid,baro,");
+  fprintf(file_logger, "baro_valid,baro,");
 #endif
 #if FILE_LOGGER_LOG_BATTERY
-    fprintf(file_logger, "v_supply,current,bat_low,bat_crit,");
+  fprintf(file_logger, "v_supply,current,bat_low,bat_crit,");
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE || \
+    FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE || \
+    FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
+  fprintf(file_logger, "percevite_ok,");
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE
+  fprintf(file_logger, "percevite_vx,percevite_vy,percevite_vz,percevite_time_since_vel,");
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE
+  fprintf(file_logger, "percevite_safe_dist,percevite_safe_dist_seq,percevite_raw_dist,percevite_valid_px,percevite_time_since_dist,");
+#endif
+#if FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
+  fprintf(file_logger, "percevite_wp_id,percevite_wp_x,percevite_wp_y,percevite_wp_z,percevite_tgt_id,percevite_tgt_x,percevite_tgt_y,percevite_tgt_z,");
+#endif
+  fprintf(file_logger, "\n");
+
+  /* Write log */
+  while(log_thread_should_run) {
+    log_frame_requested = TRUE;
+    while(log_frame_requested) pthread_yield(); // Spin until frame is filled in by periodic
+    fprintf(file_logger, "%u,%f,%f,",
+        log_frame.counter, log_frame.time, log_frame.dt);
+#if FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
+    fprintf(file_logger, "%u,%u,",
+        log_frame.block, log_frame.stage);
+#endif
+#if FILE_LOGGER_LOG_AUTOPILOT
+    fprintf(file_logger, "%u,%d,%d,%d,%d,",
+        log_frame.ap_mode, log_frame.ap_motors_on,
+        log_frame.ap_kill_throttle, log_frame.ap_in_flight,
+        log_frame.ap_ground_detected);
+#endif
+#if FILE_LOGGER_LOG_ATTITUDE
+    fprintf(file_logger, "%f,%f,%f,",
+        log_frame.att.phi, log_frame.att.theta, log_frame.att.psi);
+#endif
+#if FILE_LOGGER_LOG_LTP_POS
+    fprintf(file_logger, "%f,%f,%f,",
+        log_frame.ltp_pos.x, log_frame.ltp_pos.y, log_frame.ltp_pos.z);
+#endif
+#if FILE_LOGGER_LOG_LTP_VEL
+    fprintf(file_logger, "%f,%f,%f,",
+        log_frame.ltp_vel.x, log_frame.ltp_vel.y, log_frame.ltp_vel.z);
+#endif
+#if FILE_LOGGER_LOG_BODY_VEL
+    fprintf(file_logger, "%f,%f,%f,",
+        log_frame.body_vel.x, log_frame.body_vel.y, log_frame.body_vel.z);
+#endif
+#if FILE_LOGGER_LOG_SONAR_BEBOP
+    fprintf(file_logger, "%f,",
+        log_frame.sonar_bebop);
+#endif
+#if FILE_LOGGER_LOG_BARO
+    fprintf(file_logger, "%d,%f,",
+        log_frame.baro_valid, log_frame.baro);
+#endif
+#if FILE_LOGGER_LOG_BATTERY
+    fprintf(file_logger, "%f,%f,%d,%d,",
+        log_frame.vsupply, log_frame.current,
+        log_frame.bat_low, log_frame.bat_crit);
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE || \
   FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE || \
   FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
-    fprintf(file_logger, "percevite_ok,");
+    fprintf(file_logger, "%d,",
+        log_frame.percevite_ok);
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE
-    fprintf(file_logger, "percevite_vx,percevite_vy,percevite_vz,percevite_time_since_vel,");
+    fprintf(file_logger, "%f,%f,%f,%f,",
+        log_frame.percevite_vel.x,
+        log_frame.percevite_vel.y,
+        log_frame.percevite_vel.z,
+        log_frame.percevite_time_since_vel);
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE
-    fprintf(file_logger, "percevite_safe_dist,percevite_safe_dist_seq,percevite_raw_dist,percevite_valid_px,percevite_time_since_dist,");
+    fprintf(file_logger, "%f,%u,%f,%f,%f,",
+        log_frame.percevite_safe_dist,
+        log_frame.percevite_safe_dist_seq,
+        log_frame.percevite_raw_dist,
+        log_frame.percevite_valid_px,
+        log_frame.percevite_time_since_dist);
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
-    fprintf(file_logger, "percevite_wp_id,percevite_wp_x,percevite_wp_y,percevite_wp_z,percevite_tgt_id,percevite_tgt_x,percevite_tgt_y,percevite_tgt_z,");
+    fprintf(file_logger, "%u,%f,%f,%f,%u,%f,%f,%f,",
+        log_frame.percevite_wp_id,
+        log_frame.percevite_wp_pos.x,
+        log_frame.percevite_wp_pos.y,
+        log_frame.percevite_wp_pos.z,
+        log_frame.percevite_tgt_id,
+        log_frame.percevite_tgt_pos.x,
+        log_frame.percevite_tgt_pos.y,
+        log_frame.percevite_tgt_pos.z);
 #endif
-    fprintf(file_logger, "file_logger_time\n");
+    fprintf(file_logger, "\n");
+  }
+
+  /* Close file */
+  fclose(file_logger);
+  log_thread_is_running = FALSE;
+  return NULL;
+}
+
+
+/** Start the file logger and open a new file */
+void file_logger_start(void)
+{
+  if(!log_thread_is_running) {
+    log_thread_should_run = TRUE;
+    log_thread_is_running = TRUE;
+    pthread_create(&log_thread, NULL, &file_logger_thread, NULL);
   }
 }
 
 /** Stop the logger an nicely close the file */
 void file_logger_stop(void)
 {
-  if (file_logger != NULL) {
-    fclose(file_logger);
-    file_logger = NULL;
-  }
+  log_thread_should_run = FALSE;
 }
 
 /** Log the values to a csv file */
 void file_logger_periodic(void)
 {
-  if (file_logger == NULL) {
-    return;
-  }
-  static uint32_t counter;
-  float start = get_sys_time_float();
-  fprintf(file_logger, "%u,%f,", counter, start);
+  static float time = 0;
+  static uint32_t counter = 0;
+
+  counter++;
+  float prev_time = time;
+  time = get_sys_time_float();
+
+  if(log_frame_requested) {
+    log_frame.counter = counter;
+    log_frame.time = time;
+    log_frame.dt = time - prev_time;
 #if FILE_LOGGER_LOG_FLIGHTPLAN_BLOCK_STAGE
-  {
-    fprintf(file_logger, "%u,%u,", nav_block, nav_stage);
-  }
+    log_frame.block = nav_block;
+    log_frame.stage = nav_stage;
 #endif
 #if FILE_LOGGER_LOG_AUTOPILOT
-  {
-    fprintf(file_logger, "%u,%d,%d,%d,%d,",
-        autopilot.mode, autopilot.motors_on,
-        autopilot.kill_throttle, autopilot.in_flight,
-        autopilot.ground_detected);
-  }
+    log_frame.ap_mode = autopilot.mode;
+    log_frame.ap_motors_on = autopilot.motors_on;
+    log_frame.ap_kill_throttle = autopilot.kill_throttle;
+    log_frame.ap_in_flight = autopilot.in_flight;
+    log_frame.ap_ground_detected = autopilot.ground_detected;
 #endif
 #if FILE_LOGGER_LOG_ATTITUDE
-  {
-    struct FloatEulers *att = stateGetNedToBodyEulers_f();
-    fprintf(file_logger, "%f,%f,%f,",
-        att->phi, att->theta, att->psi);
-  }
+    log_frame.att = *stateGetNedToBodyEulers_f();
 #endif
 #if FILE_LOGGER_LOG_LTP_POS
-  {
-    struct NedCoor_f *pos_ltp = stateGetPositionNed_f();
-    fprintf(file_logger, "%f,%f,%f,", pos_ltp->x, pos_ltp->y, pos_ltp->z);
-  }
+    log_frame.ltp_pos = *stateGetPositionNed_f();
 #endif
 #if FILE_LOGGER_LOG_LTP_VEL
-  {
-    struct NedCoor_f *vel_ltp = stateGetSpeedNed_f();
-    fprintf(file_logger, "%f,%f,%f,", vel_ltp->x, vel_ltp->y, vel_ltp->z);
-  }
+    log_frame.ltp_vel = *stateGetSpeedNed_f();
 #endif
 #if FILE_LOGGER_LOG_BODY_VEL
-  {
     struct FloatRMat *R = stateGetNedToBodyRMat_f();
     struct NedCoor_f *vel_ltp = stateGetSpeedNed_f();
-    struct FloatVect3 vel_body;
-    MAT33_VECT3_MUL(vel_body, *R, *vel_ltp);
-    fprintf(file_logger, "%f,%f,%f,", vel_body.x, vel_body.y, vel_body.z);
-  }
+    MAT33_VECT3_MUL(log_frame.body_vel, *R, *vel_ltp);
 #endif
 #if FILE_LOGGER_LOG_SONAR_BEBOP
-  {
-    fprintf(file_logger, "%f,", sonar_bebop.distance);
-  }
+    log_frame.sonar_bebop = sonar_bebop.distance;
 #endif
 #if FILE_LOGGER_LOG_BARO
-  {
-    fprintf(file_logger, "%d,%f,",
-        air_data.amsl_baro_valid, air_data.amsl_baro);
-  }
+    log_frame.baro_valid = air_data.amsl_baro_valid;
+    log_frame.baro = air_data.amsl_baro;
 #endif
 #if FILE_LOGGER_LOG_BATTERY
-  {
-    fprintf(file_logger, "%f,%f,%d,%d,",
-        electrical.vsupply / 10.0, electrical.current / 1000.0,
-        electrical.bat_low, electrical.bat_critical);
-  }
+    log_frame.vsupply = electrical.vsupply / 10.0;
+    log_frame.current = electrical.current / 1000.0;
+    log_frame.bat_low = electrical.bat_low;
+    log_frame.bat_crit = electrical.bat_critical;
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE || \
-  FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE || \
-  FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
-  {
-    fprintf(file_logger, "%d,", PerceviteOk());
-  }
+    FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE || \
+    FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
+    log_frame.percevite_ok = PerceviteOk();
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_VELOCITY_ESTIMATE
-  {
-    fprintf(file_logger, "%f,%f,%f,%f,",
-        percevite_logging.velocity.x,
-        percevite_logging.velocity.y,
-        percevite_logging.velocity.z,
-        percevite.time_since_velocity);
-  }
+    log_frame.percevite_vel = percevite_logging.velocity;
+    log_frame.percevite_time_since_vel = percevite.time_since_velocity;
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_SAFE_DISTANCE
-  {
-    fprintf(file_logger, "%f,%u,%f,%f,%f,",
-        percevite.safe_region.distance, percevite.safe_region.seq,
-        percevite_logging.raw_distance, percevite_logging.valid_pixels,
-        percevite.time_since_safe_distance);
-  }
+    log_frame.percevite_safe_dist = percevite.safe_region.distance;
+    log_frame.percevite_safe_dist_seq = percevite.safe_region.seq;
+    log_frame.percevite_raw_dist = percevite_logging.raw_distance;
+    log_frame.percevite_valid_px = percevite_logging.valid_pixels;
+    log_frame.percevite_time_since_dist = percevite.time_since_safe_distance;
 #endif
 #if FILE_LOGGER_LOG_PERCEVITE_WAYPOINTS
-  {
     struct NedCoor_f wp = {0.0, 0.0, 0.0};
     if(percevite.wp < nb_waypoint) {
       wp.x = waypoint_get_y(percevite.wp); // Note: convert to NED
@@ -284,12 +426,12 @@ void file_logger_periodic(void)
       tgt.y = waypoint_get_x(percevite_logging.target_wp);
       tgt.z = -waypoint_get_alt(percevite_logging.target_wp);
     }
-    fprintf(file_logger, "%u,%f,%f,%f,%u,%f,%f,%f,",
-        percevite.wp, wp.x, wp.y, wp.z,
-        percevite_logging.target_wp, tgt.x, tgt.y, tgt.z);
-  }
+    log_frame.percevite_wp_id = percevite.wp;
+    log_frame.percevite_wp_pos = wp;
+    log_frame.percevite_tgt_id = percevite_logging.target_wp;
+    log_frame.percevite_tgt_pos = tgt;
 #endif
-  float end = get_sys_time_float();
-  fprintf(file_logger, "%f\n", end - start);
-  counter++;
+
+    log_frame_requested = FALSE;
+  }
 }
