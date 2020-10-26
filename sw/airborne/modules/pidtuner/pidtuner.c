cf5947eddd169a283495e21a2cdeeb824c9f5a5a
Tom van Dijk <tomvand@users.noreply.github.com>
2020-10-26 10:21:34 +0100

    Check out missing files
    Pidtuner module
    Radio with GAIN channels

---
+++ sw/airborne/modules/pidtuner/pidtuner.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) Tom van Dijk
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/pidtuner/pidtuner.c"
+ * @author Tom van Dijk
+ * tune pid gains via rc
+ */
+
+#include "modules/pidtuner/pidtuner.h"
+
+#include "subsystems/radio_control.h"
+#include "firmwares/rotorcraft/stabilization/stabilization_attitude_quat_int.h"
+#include "firmwares/rotorcraft/guidance/guidance_v.h"
+#include "firmwares/rotorcraft/guidance/guidance_h.h"
+#include "subsystems/ahrs.h"
+
+void pidtuner_periodic(void) {
+  float gain1 = radio_control.values[RADIO_GAIN1];
+  float gain2 = radio_control.values[RADIO_GAIN2];
+
+  // Gain Kp
+  guidance_h.gains.p = (gain1 / 9600.0) * 2 * 650;
+
+  // Gain Kd
+  guidance_h.gains.d = (gain2 / 9600.0) * 2 * 350;
+
+
+//  // Differential time Td
+//  stabilization_gains.d.x = stabilization_gains.p.x * ((gain2 / 9600.0) * 2 * 1.0);
+//  stabilization_gains.d.y = stabilization_gains.p.y * ((gain2 / 9600.0) * 2 * 1.0);
+
+}
+
+
