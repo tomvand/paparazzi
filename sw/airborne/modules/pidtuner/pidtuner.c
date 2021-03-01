8ec460641d78f6c515bb256b92b8102311df235c
Tom van Dijk <tomvand@users.noreply.github.com>
2021-03-01 15:37:27 +0100

    Also keep PI zero at same frequency

--- sw/airborne/modules/pidtuner/pidtuner.c
+++ sw/airborne/modules/pidtuner/pidtuner.c
@@ -74,6 +74,7 @@ static void __attribute__((unused)) pid_periodic(void) {
   float gain2 = radio_control.values[RADIO_GAIN2] / 9600.0 * 2;
 
   stabilization_gains.p.x = 50.0 *   1.0 * gain2;
+  stabilization_gains.i.x =  5.0 *   1.0 * gain2;
   stabilization_gains.d.x = 40.0 * gain1 * gain2;
 }
 
