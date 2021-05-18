459f748ff5bd20325f6a530d04efe64ec796a5be
Tom van Dijk <tomvand@users.noreply.github.com>
2021-05-18 14:02:30 +0200

    Log homing vector and variables

--- sw/airborne/modules/visualhoming/visualhoming.c
+++ sw/airborne/modules/visualhoming/visualhoming.c
@@ -29,6 +29,7 @@
 #include "firmwares/rotorcraft/navigation.h"
 #include "subsystems/navigation/waypoints.h"
 #include "subsystems/abi.h"
+#include "subsystems/datalink/telemetry.h"
 #include "math/pprz_algebra_int.h"
 
 #ifndef RELATIVE_LOCALIZATION_ID
@@ -40,32 +41,63 @@
 #endif
 
 #ifndef HOMING_WAYPOINT
-#define HOMING_WAYPOINT WP_HOMING
+#define HOMING_WAYPOINT WP_HOMING_VISUAL
 #endif
 
 static abi_event relative_localization_ev;
 
+
 static struct {
+  int32_t vector_id;
+  struct EnuCoor_f target;
   struct EnuCoor_f vector;
+  struct EnuCoor_f ins;
   bool is_new_data;
 } homing;
+
+static struct {
+  int32_t vector_id;
+  struct EnuCoor_f target;
+  struct EnuCoor_f vector;
+  struct EnuCoor_f ins;
+  bool is_new_data;
+} telemetry;
+
+
+static void send_visualhoming(struct transport_tx *trans, struct link_device *dev) {
+  if (telemetry.is_new_data) {
+    pprz_msg_send_VISUALHOMING(trans, dev, AC_ID,
+        &telemetry.vector_id,
+        &telemetry.target.x, &telemetry.target.y, &telemetry.target.z,
+        &telemetry.vector.x, &telemetry.vector.y, &telemetry.vector.z,
+        &telemetry.ins.x, &telemetry.ins.y, &telemetry.ins.z);
+    telemetry.is_new_data = FALSE;
+  }
+}
 
 static void relative_localization_cb(uint8_t sender_id __attribute__((unused)), int32_t id __attribute__((unused)),
     float x, float y, float z,
     float vx __attribute__((unused)), float vy __attribute__((unused)), float vz __attribute__((unused)))
 {
+  homing.vector_id = id;
   homing.vector.x = x;
   homing.vector.y = y;
   homing.vector.z = z;
+
+  homing.ins = *stateGetPositionEnu_f();
+  struct EnuCoor_f vec;
+  VECT3_SMUL(vec, homing.vector, HOMING_VECTOR_SCALE);
+  VECT3_SUM(homing.target, homing.ins, vec)
+
   homing.is_new_data = TRUE;
 }
 
 void visualhoming_init(void)
 {
-  // your init code here
-
-  // Abi messages bindings
   AbiBindMsgRELATIVE_LOCALIZATION(RELATIVE_LOCALIZATION_ID, &relative_localization_ev, relative_localization_cb);
+#if PERIODIC_TELEMETRY
+  register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_VISUALHOMING, send_visualhoming);
+#endif // PERIODIC_TELEMETRY
 }
 
 void visualhoming_periodic(void)
@@ -80,20 +112,21 @@ void visualhoming_periodic(void)
 bool NavHoming(void) {
   // Update guidance vector
   if (homing.is_new_data) {
-    struct EnuCoor_f *pos = stateGetPositionEnu_f();
-    struct EnuCoor_f vec;
-    struct EnuCoor_f tgt;
     struct EnuCoor_i tgt_i;
-    VECT3_SMUL(vec, homing.vector, HOMING_VECTOR_SCALE);
-    VECT3_SUM(tgt, *pos, vec)
-    tgt_i.x = POS_BFP_OF_REAL(tgt.x);
-    tgt_i.y = POS_BFP_OF_REAL(tgt.y);
-    tgt_i.z = POS_BFP_OF_REAL(tgt.z);
+    tgt_i.x = POS_BFP_OF_REAL(homing.target.x);
+    tgt_i.y = POS_BFP_OF_REAL(homing.target.y);
+    tgt_i.z = POS_BFP_OF_REAL(homing.target.z);
     waypoint_move_enu_i(HOMING_WAYPOINT, &tgt_i);
     NavGotoWaypoint(HOMING_WAYPOINT);
     homing.is_new_data = FALSE;
+    // Copy data for telemetry
+    telemetry.vector_id = homing.vector_id;
+    telemetry.target = homing.target;
+    telemetry.vector = homing.vector;
+    telemetry.ins = homing.ins;
+    telemetry.is_new_data = TRUE;
   }
-  // Arrival detection
+  // TODO Arrival detection
   return TRUE;
 }
 
